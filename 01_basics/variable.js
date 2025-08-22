3e573e505a431659b775055e952cc60c5a87c227
Shivesh Kumar <160155797+Shivesh7856@users.noreply.github.com>
2025-08-22 05:30:11 +0000

    config

---
+++ 01_basics/variable.js
@@ -0,0 +1,5 @@
+const accountID = 14453;
+let accountEmail ="shiveshkumar822@gmail.com";
+var accountPassoward = "122334";
+account_city="jaipur";
+console.table([accountID,accountEmail,accountPassoward,account_city]);
