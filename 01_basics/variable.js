dd49fc44e236d798c8e0e512ca6a0ea964a778d3
Shivesh Kumar <160155797+Shivesh7856@users.noreply.github.com>
2025-08-22 05:55:15 +0000

    config

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -3,3 +3,4 @@ let accountEmail ="shiveshkumar822@gmail.com";
 var accountPassoward = "122334";
 account_city="jaipur";
 console.table([accountID,accountEmail,accountPassoward,account_city]);
+/*prefer not to use var because of issue in block scope and functional scope */
