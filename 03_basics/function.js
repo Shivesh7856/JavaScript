edb881cfc4777bf1a723ded62c9914136ea31b99
Shivesh Kumar <160155797+Shivesh7856@users.noreply.github.com>
2025-08-23 12:11:42 +0000

    function & scope

---
+++ 03_basics/function.js
@@ -0,0 +1,13 @@
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+//     }
+// const result=addTwoNumbers(20,30);
+// console.log("result",result);
+// function loginUserMessage(username){
+//     return '${username} justlogin'
+// }
+// console.log(loginUserMessage("shivesh"));
+function calculatecardprice(...num1){
+    return num1;
+}
+console.log(calculatecardprice(200,400,500));
---
+++ 03_basics/scope.js
@@ -0,0 +1,62 @@
+console.log(addone(5))
+ function addone(num){
+     return num+2;
+    }
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "shivesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "shivesh"
+    if (username === "shivesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
